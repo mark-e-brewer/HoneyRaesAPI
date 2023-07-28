f50c4aa2e56dccfc7a08e6ee6f206cdf38398329
Mark Brewer <markbrewer369@gmail.com>
2023-07-28 00:02:56 -0500

    Emergency endpoints and connecting list for GETs

--- Models/ServiceTicket.cs
+++ Models/ServiceTicket.cs
@@ -5,11 +5,15 @@ public class ServiceTicket
 
     public int CustomerId { get; set; }
 
-    public int EmployeeId { get; set; }
+    public int? EmployeeId { get; set; }
 
     public string Description { get; set; }
 
-    public bool Emergency { get; set; }
+    public bool? Emergency { get; set; }
 
     public string DateComplete { get; set; }
+
+    public Employee Employee { get; set; }
+
+    public Customer Customer { get; set; }
 }
